03275cc66bf95d552b19d5aeac38f04eee63df02
Pedro de Medeiros <pedro.medeiros@gmail.com>
2025-01-03 22:18:25 -0300

    add about button

--- raygui-app/main.c
+++ raygui-app/main.c
@@ -5,31 +5,33 @@
 
 int main()
 {
-    InitWindow(800, 800, "raygui - controls test suite");
-    SetTargetFPS(60);
+	InitWindow(800, 800, "raygui - controls test suite");
+	SetTargetFPS(60);
 
-    bool showMessageBox = false;
+	bool fileLoaded = false;
+	bool showAboutBox = false;
 
-    while (!WindowShouldClose())
-    {
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-            if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+		if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Button"))
+			fileLoaded = true;
+		if (GuiButton((Rectangle){ 24, 70, 120, 30 }, "#191#About"))
+			showAboutBox = true;
 
-            if (showMessageBox)
-            {
-                int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
-                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+		if (showAboutBox)
+		{
+			int result = GuiMessageBox((Rectangle){ 275, 350, 250, 100 },
+			        "#191#About", "Hi! This is a message!", "OK");
 
-                if (result >= 0) showMessageBox = false;
-            }
+			if (result >= 0) showAboutBox = false;
+		}
 
-        EndDrawing();
-    }
+		EndDrawing();
+	}
 
-    CloseWindow();
-    return 0;
+	CloseWindow();
+	return 0;
 }
